8fe66f74cb53a2d89b6d1d0adea2d508cf29b2ad
Tutor-DA <support@tutorda.org>
2025-06-05 17:15:44 +0400

    faq java file added

---
+++ assets/js/faq-toggle.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+      const answer = button.nextElementSibling;
+      answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+    });
+  });
+});
